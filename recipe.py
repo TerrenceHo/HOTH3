5c6107df1a6e58d25468340f64bfdc06df481c9d
kensterino <kevin231998@gmail.com>
2017-11-12 19:50:13 -0800

    fixed get_data

--- recipe.py
+++ recipe.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 base_url = "http://forkthecookbook.com"
 search_base_url = "http://forkthecookbook.com/search-recipes"
-specific_url = "http://www.forkthecookbook.com/recipes/braised-chicken-tomatoes-potatoes-peas-1bafa"
+specific_url = "http://www.forkthecookbook.com/recipes/chicago-pizza-burgers-eac03"
 
 def get_recipe_names(search_term):
     names_list = []
@@ -33,23 +33,30 @@ def get_recipe_URLs(search_term):
         links_list.append(base_url+soup[i]['href'])
     return links_list
 
-def get_recipe_ingredients(specific_url):
+def get_data(specific_url):
     ingredients_list = []
+    instructions_list = []
     r = requests.get(specific_url)
     soup = BeautifulSoup(r.text, "html.parser")
+
     ingredients_list = soup.find_all("td", class_="recipe-ingredients")
     for i in range(len(ingredients_list)):
         ingredients_list[i] = " ".join(ingredients_list[i].get_text().strip().split())
-    return ingredients_list
+    
+    instructions_list = soup.find("td", class_="recipe-instructions").find_all("li")
+    for i in range(len(instructions_list)):
+        instructions_list[i] = " ".join(instructions_list[i].get_text().strip().split())    #size of 1
 
-#need instructions
+
+    return (ingredients_list, instructions_list)
 
 if __name__ == "__main__":
     #links_list = get_recipe_URLs("chicken")
     #print(links_list)
     #names_list = get_recipe_names("chicken")
     #print(names_list)
-    ingredients_list = get_recipe_ingredients("http://www.forkthecookbook.com/recipes/braised-chicken-tomatoes-potatoes-peas-1bafa")
-    print(ingredients_list)
-    #get_single_recipie_name(specific_url)
+    ingredients_list, instructions_list = get_data(specific_url)
 
+    print(instructions_list)
+
+    #get_single_recipie_name(specific_url)
